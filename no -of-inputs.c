193a133e04a4bdbe3483a46c46e421570a80b005
shivani12c0 <85931939+shivani12c0@users.noreply.github.com>
2021-07-19 23:11:03 +0530

    Add files via upload
    no-of-inputs

---
+++ no -of-inputs.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{ 
+        int num,count=0;
+        while(num!=0)
+        {
+        	
+         printf("enter num");
+         scanf("%d",&num);
+         count=count+1;
+         
+}
+         printf("%d",count-1);
+}
