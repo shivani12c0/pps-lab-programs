4b705058bbbccb865f2e1d3ed06869ae43f8ff3b
shivani12c0 <85931939+shivani12c0@users.noreply.github.com>
2021-07-19 22:36:13 +0530

    Add files via upload
    check whether a number is postive or negative.txt

---
+++ 6.cpp
@@ -0,0 +1,15 @@
+/* 2.check whwther a number is postive or negative*/
+
+#include<studio.h>
+int main()
+{
+	     int num ;
+    printf("Enter a number ");
+         scanf("%d",&num);
+         if(num > 0)
+         printf("positive number");
+         else if(num< 0)
+         printf("negative number");
+         else
+         printf("zero");
+}
