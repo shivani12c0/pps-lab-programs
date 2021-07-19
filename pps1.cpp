88dae17af27a5d879414e7d8f4c50c78779ec58e
shivani12c0 <85931939+shivani12c0@users.noreply.github.com>
2021-07-19 21:47:23 +0530

    Add files via upload
    program to print 5table.txt

---
+++ pps1.cpp
@@ -0,0 +1,15 @@
+//program to print 5 table
+
+#include<studio.h>
+int main()
+{
+	int num=5,i;
+	print("enter a number:");
+	scanf("%d",&num);
+	    for(i=1;i<=10;i++)
+	    {
+	    	     printf("%d * %d = %d\n",num,i,num*i);
+		}
+		
+		
+}
