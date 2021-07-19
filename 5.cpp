b2325d9a0aaac7eb32e253bf31ac756d32d0ae9f
shivani12c0 <85931939+shivani12c0@users.noreply.github.com>
2021-07-19 22:28:56 +0530

    Add files via upload
    sum-array-elements.txt

---
+++ 5.cpp
@@ -0,0 +1,18 @@
+//sum of array elements
+
+#include<studio.h>
+int main()
+{
+	     int A[10],i,sum=0;
+	              float avg;
+	     print("enter 12 numbers\n");
+	     for(i=0;i<10;i++)
+	        scanf("%d",&A[i]]);
+	       printf("\n");
+	       for(i=0;i<10;i++)
+	       {
+	       	sum +=A[i];		  
+		    }
+		      printf("%d",sum);
+		    return 1;
+}
