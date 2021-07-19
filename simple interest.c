e29d35cc1c8c105ae677ab09b0c3e9898a0a9616
shivani12c0 <85931939+shivani12c0@users.noreply.github.com>
2021-07-19 23:16:49 +0530

    Add files via upload
    simple interest.c

---
+++ simple interest.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{ 
+        int P,T,R,SI;
+        printf("\n enter values");
+        scanf("\n%d%d%d",&P&T,&R);
+        SI=(P*T*R)/100;
+        printf("\nSI=%d",SI);
+}
