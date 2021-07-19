a57fb09c6a41a5de22096d2ee1d5f1d0c74b1887
shivani12c0 <85931939+shivani12c0@users.noreply.github.com>
2021-07-19 22:03:22 +0530

    Add files via upload
    sum-of- n-integers

---
+++ pps3.cpp
@@ -0,0 +1,13 @@
+/* 9. program to find sum of n integers Eg: sum of 10 integers= 1+2+3+4+.....10=55*/
+
+#include<studio.h>
+int main()
+{
+	     int num,sum;
+	     print("enter a number:\n");
+	     scanf("%d",&num);
+	     sum = (num*(num+1))/;
+	     printf("the sum of digits is %d",sum);
+	     return 0;
+}
+
