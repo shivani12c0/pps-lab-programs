6f053ff2615e354e808613d3bff39eba797a1cb7
shivani12c0 <85931939+shivani12c0@users.noreply.github.com>
2021-07-19 23:21:50 +0530

    Add files via upload
    time conversion.c

---
+++ time conversion.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{ 
+        int sec,min,hrs;
+        printf("\nEnter no.of sec");
+        scanf("\n%d",&sec);
+        hrs=(sec/3600);
+        min=(sec/60);
+        printf("\nhrs=%d,min=%",hrs,min);
+}
